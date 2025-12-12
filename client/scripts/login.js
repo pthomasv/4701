a9c43124567a5110bbbe1941fb174ab78d66ed92
vvvetroff <creatorcax@gmail.com>
2025-12-12 10:19:46 -0500

    feat: login interaction

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,2 +1,29 @@
-function handleLogin(){
+async function handleLogin(event){
+  event.preventDefault()
+
+  console.log("asd")
+
+  const email = document.getElementById("fieldEmail").value
+  const password = document.getElementById("fieldPassword").value
+
+  const data = { email, password }
+
+  try {
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    const result = await response.json()
+    if (result.success) {
+      localStorage.setItem('user')
+    }
+
+
+
+  } catch (error) {
+    console.error('Login error:', error)
+  }
 }
--- server/server.js
+++ server/server.js
@@ -7,11 +7,58 @@ const corsOptions ={
     origin: ["http://127.0.0.1:5501", "http://127.0.0.1:5500"] //onlt accepts requests from these port
 }
 app.use(cors(corsOptions))
+app.use(express.json())
 
 app.get('/test', (req, res) => {
     res.json("hello world");
   
 });
+
+/* Login endpoint*/
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  const db = new sqlite3.Database('./db/app_database.db')
+  db.get("SELECT * FROM User WHERE email = ? AND password = ?",
+    [email, password],
+    (err, u) => {
+      if (err) {
+        res.status(500).json({ success: false, message: err.message })
+        db.close()
+        return
+      }
+      if (!u) {
+        res.status(401).json({ success: false, message: "Invalid login" })
+        db.close()
+        return
+      }
+      if (u.role === "customer") {
+        db.get("SELECT * FROM Customer WHERE userID = ?",
+          [u.userID],
+          (err, c) => {
+            if (err) {
+              res.status(500).json({ success: false, message: err.message })
+            } else {
+              res.json({ success: true, user: {userID: u.userID, email: u.email, role: u.role, name: c.name, address: c.address, phone: c.phone }})
+            }
+            db.close()
+          }
+        )
+      } else if (user.role === "employee") {
+        db.get("SELECT * FROM Employee WHERE userID = ?",
+          [u.userID],
+          (err, e) => {
+            if (err) {
+              res.status(500).json({ success: false, message: err.message })
+            } else {
+              res.json({ success: true, user: {userID: u.userID, email: u.email, role: u.role, name: e.name, dealerID: e.dealerID }})
+            }
+            db.close()
+          }
+        )
+      }
+    }
+  )
+})
 
 app.get('/Customer', (req, res) => {
   const db = new sqlite3.Database('./db/app_database.db');
